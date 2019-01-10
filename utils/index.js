f42cf7a835e40760a944db0c8eda95a32835c6c4
pumelotea <535553297@qq.com>
2019-01-10 09:30:00 +0800

    fix bug

--- utils/index.js
+++ utils/index.js
@@ -2,8 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const spawn = require('child_process').spawn
 
-const lintStyles = ['standard', 'airbnb']
-
 /**
  * Sorts dependencies in package.json alphabetically.
  * They are unsorted because they were grouped for the handlebars helpers
@@ -18,47 +16,6 @@ exports.sortDependencies = function sortDependencies(data) {
   packageJson.devDependencies = sortObject(packageJson.devDependencies)
   packageJson.dependencies = sortObject(packageJson.dependencies)
   fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2) + '\n')
-}
-
-/**
- * Runs `yarn install` in the project directory
- * @param {string} cwd Path of the created project directory
- * @param {object} data Data from questionnaire
- */
-exports.installDependencies = function installDependencies(
-  cwd,
-  executable = 'yarn',
-  color
-) {
-  console.log(`\n\n# ${color('Installing project dependencies ...')}`)
-  console.log('# ========================\n')
-  return runCommand(executable, ['install'], {
-    cwd,
-  })
-}
-
-/**
- * Runs `npm run lint -- --fix` in the project directory
- * @param {string} cwd Path of the created project directory
- * @param {object} data Data from questionnaire
- */
-exports.runLintFix = function runLintFix(cwd, data, color) {
-  if (data.lint && lintStyles.indexOf(data.lintConfig) !== -1) {
-    console.log(
-      `\n\n${color(
-        'Running eslint --fix to comply with chosen preset rules...'
-      )}`
-    )
-    console.log('# ========================\n')
-    const args =
-      data.autoInstall === 'npm'
-        ? ['run', 'lint', '--', '--fix']
-        : ['run', 'lint', '--fix']
-    return runCommand(data.autoInstall, args, {
-      cwd,
-    })
-  }
-  return Promise.resolve()
 }
 
 /**
@@ -87,28 +44,6 @@ To get started:
   
 `
   console.log(message)
-}
-
-/**
- * If the user will have to run lint --fix themselves, it returns a string
- * containing the instruction for this step.
- * @param {Object} data Data from questionnaire.
- */
-function lintMsg(data) {
-  return !data.autoInstall &&
-    data.lint &&
-    lintStyles.indexOf(data.lintConfig) !== -1
-    ? 'npm run lint -- --fix (or for yarn: yarn run lint --fix)\n  '
-    : ''
-}
-
-/**
- * If the user will have to run `npm install` or `yarn` themselves, it returns a string
- * containing the instruction for this step.
- * @param {Object} data Data from the questionnaire
- */
-function installMsg(data) {
-  return !data.autoInstall ? 'yarn install\n  ' : ''
 }
 
 /**
